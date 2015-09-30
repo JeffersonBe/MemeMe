d846b99a212c51b2dc0542e37d9549f80b2c2484
Jefferson <jefferson.bonnaire@gmail.com>
2015-09-30 23:14:04 +0100

    Add keyboard management

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var ImagePickerView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if (ImagePickerView.image == nil) {
+            topTextField.hidden = true
+            bottomTextField.hidden = true
+        }
+
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName : UIColor.blackColor(), //TODO: Fill in appropriate UIColor,
+            NSForegroundColorAttributeName : UIColor.clearColor(), //TODO: Fill in UIColor,
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : 1.0 //TODO: Fill in appropriate Float
+        ]
+
+        self.topTextField.delegate = self
+        self.bottomTextField.delegate = self
+        
+        topTextField.text = "Enter your Text Here"
+        bottomTextField.text = "Enter your Text Here"
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,10 +55,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             self.ImagePickerView.image = image
         }
+        topTextField.hidden = false
+        bottomTextField.hidden = false
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        topTextField.resignFirstResponder()
+        bottomTextField.resignFirstResponder()
+        return true
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
 }
